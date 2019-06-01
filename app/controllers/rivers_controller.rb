b9e45c8f3373605d73f135951bde93d2f1645efd
Angappan <482043@AMB01102.local>
2019-06-01 13:40:53 +0530

    pipeline sample

--- app/controllers/rivers_controller.rb
+++ app/controllers/rivers_controller.rb
@@ -1,6 +1,7 @@
 class RiversController < ApplicationController
   def index
     puts "I am in the river controller"
+    puts " I am inside the river controller "
     render json: RestClient.get("http://riverbrain.com/api/v1/rivers", {})
   end
 end
